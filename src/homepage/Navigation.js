23a90418300c860dd37568d08537e0d282240cd3
Adrian Kawanishi <adriankawanishi@gmail.com>
2019-03-19 12:01:07 -0700

    Finish navigation functionality

--- src/homepage/Navigation.js
+++ src/homepage/Navigation.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react'
 
 export default class Navigation extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checked: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({checked: false})
+  }
+
+  toggle() {
+    this.setState({
+      checked: !this.state.checked
+    })
+  }
+
   render() {
+    const items = [['About Us', 'about'], ['Features', 'features'], ['Languages', 'lang'], ['Reviews', 'reviews'], ['Book Now', 'booking']];
+    const listItems = items.map(([text, link]) => (
+      <li className="navigation__item"
+           onClick={() => this.toggle()}
+      >
+        <a href={`#section-${link}`} className="navigation__link">
+        <span>01</span>{text}</a>
+      </li>
+    ));
+
     return (
       <div className="navigation">
-        <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-        <label htmlFor="navi-toggle" className="navigation__button">
-          <span className="navigation__icon">&nbsp;</span>
+        <input type="checkbox" className="navigation__checkbox" id="navi-toggle" checked={this.state.checked}/>
+        <label htmlFor="navi-toggle" className="navigation__button" onClick={() => this.toggle()}>
+        <span className="navigation__icon">&nbsp;</span>
         </label>
         <div className="navigation__background">&nbsp;</div>
         <nav className="navigation__nav">
           <ul className="navigation__list">
-            <li className="navigation__item"><a href="#section-about" className="navigation__link"><span>01</span>About Us</a></li>
-            <li className="navigation__item"><a href="#section-features" className="navigation__link"><span>02</span>features</a></li>
-            <li className="navigation__item"><a href="#section-lang" className="navigation__link"><span>03</span>Languages</a></li>
-            <li className="navigation__item"><a href="#section-reviews" className="navigation__link"><span>04</span>reviews</a></li>
-            <li className="navigation__item"><a href="#section-booking" className="navigation__link"><span>05</span>Book Now</a></li>
+            {listItems}
           </ul>
         </nav>
       </div>
